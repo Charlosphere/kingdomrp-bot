693412b794db76df3259bcbfee5910b9fc901b12
wcote <william.cote@ticketmaster.com>
2017-04-28 16:41:17 -0400

    WIP $store command

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const emoji = require('node-emoji');
 const moment = require('moment');
 const database = require('./database');
 const config = require('./config.json');
@@ -22,8 +23,19 @@ bot.on('message', msg => {
 
   // Help command
   if (command === 'help') {
-    console.log(`[${timestamp()}] ${msg.author.username} used $help command.`);
+    console.log(`[${timestamp()}] ${msg.author.username} used ${config.trigger}help command.`);
     msg.reply('Here are my commands.');
+  }
+
+  // Store command
+  if (command === 'store') {
+      console.log(`[${timestamp()}] ${msg.author.username} used ${config.trigger}store command.`);
+      msg.reply(`Here\'s what you can buy:\n
+                ${emoji.get('dagger')} Donne +1 chance de tuer (2 000 ${emoji.get('moneybag')})\n
+                ${emoji.get('shield')} Donne +1 chance de survivre (2 000 ${emoji.get('moneybag')})\n
+                ${emoji.get('bow_and_arrow')} Permet de tuer à partir de la prison (3 000 ${emoji.get('moneybag')})\n
+                ${emoji.get('bomb')} Permet de tuer deux personne avec un roll (5 000 ${emoji.get('moneybag')})\n
+                ${emoji.get('four_leaf_clover')} Permet de roll (20 000 ${emoji.get('moneybag')})\n`);
   }
 });
 
