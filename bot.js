cede1b59792bbd96423008ca144a191947728347
Vincent Dumont <dumontv@outlook.com>
2017-04-29 16:23:23 -0400

    Completed admin and suadmin commands

--- bot.js
+++ bot.js
@@ -27,30 +27,89 @@ bot.on('guildMemberRemove', member => {
 
 bot.on('message', msg => {
 
+  // #### PUBLIC COMMANDS ####
+
   // Help command
   if (msg.content.startsWith(`${config.trigger}help`)) {
     console.log(`[${timestamp()}] ${msg.author.username} used ${config.trigger}help command.`);
     msg.reply('Here are my commands.');
   }
 
+  // ########################
+
+  // #### ADMIN COMMANDS ####
+
   // Super-user give: magically give gold to any player
-  else if (msg.content.startsWith('$sugive')) {
+  else if (msg.content.startsWith(`${config.trigger}sugive`)) {
     database.getPromotion(msg.author.id, (err, row) => {
-      // If he's an admin or a super admin...
       if (row.admin === 1 || row.suadmin === 1) {
-        // If a user is mentionned in the message...
         if (msg.mentions.users.first() !== undefined) {
-          // If the message contains a number...
-          let match = /\s\d+/.exec(msg.content);
+          let match = /\s\d+/.exec(msg.content); // regex: contains a number with whitespace before
           if (match !== null) {
             database.getGold(msg.mentions.users.first().id, (err, row) => {
               database.updateGold(msg.mentions.users.first().id, row.gold + parseInt(match[0]))
+              msg.reply(`You magically transfered ${parseInt(match[0])} ${emoji.get('dollar')} to ${msg.mentions.users.first().username}.`);
             });
           }
         }
       }
     });
   }
+
+  // Reset user: set gold to 0 (eventually reset inventory as well)
+  else if (msg.content.startsWith(`${config.trigger}reset`)) {
+    database.getPromotion(msg.author.id, (err, row) => {
+      if (row.admin === 1 || row.suadmin === 1) {
+        if (msg.mentions.users.first() !== undefined) {
+          database.updateGold(msg.mentions.users.first().id, 0);
+          msg.reply(`You reset ${msg.mentions.users.first().username}. He now has 0 ${emoji.get('dollar')}.`);
+        }
+      }
+    });
+  }
+
+  // ########################
+
+  // #### SUPER-ADMIN COMMANDS ####
+
+  // Promote user: Add as admin
+  else if (msg.content.startsWith(`${config.trigger}promote`)) {
+    database.getPromotion(msg.author.id, (err, row) => {
+      if (row.suadmin === 1) {
+        if (msg.mentions.users.first() !== undefined) {
+          database.getPromotion(msg.mentions.users.first().id, (err, row) => {
+            if (row.admin === 0) {
+              database.promoteUser(msg.mentions.users.first().id, 1);
+              msg.reply(`You promoted ${msg.mentions.users.first().username} as admin.`)
+            }
+            else {
+              msg.reply(`${msg.mentions.users.first().username} is already an admin.`)
+            }
+          });
+        }
+      }
+    });
+  }
+
+  // Demote user: Remove from admins
+  else if (msg.content.startsWith(`${config.trigger}demote`)) {
+    database.getPromotion(msg.author.id, (err, row) => {
+      if (row.suadmin === 1) {
+        if (msg.mentions.users.first() !== undefined) {
+          database.getPromotion(msg.mentions.users.first().id, (err, row) => {
+            if (row.admin === 1) {
+              database.promoteUser(msg.mentions.users.first().id, 0);
+              msg.reply(`You demoted ${msg.mentions.users.first().username}. He no longer is an admin.`)
+            }
+            else {
+              msg.reply(`${msg.mentions.users.first().username} is not an admin.`)
+            }
+          });
+        }
+      }
+    });
+  }
+
 });
 
 bot.login(token);
