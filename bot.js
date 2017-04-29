eb51593e25a6d5606545015e6be3e680dff4859d
Vincent Dumont <dumontv@outlook.com>
2017-04-28 20:41:48 -0400

    Users are now registered to database

--- bot.js
+++ bot.js
@@ -2,13 +2,21 @@ const Discord = require('discord.js');
 const moment = require('moment');
 const database = require('./database');
 
+const timestamp = () => moment(new Date).format('HH:mm:ss');
 const token = process.env.KINGDOMRP_TOKEN;
 const bot = new Discord.Client();
-const timestamp = () => moment(new Date).format('HH:mm:ss');
 
 bot.on('ready', () => {
-  console.log(`Logged in as ${bot.user.username} at [${timestamp()}]!`);
-  database.init();
+  console.log(`${bot.user.username} launched at [${timestamp()}]!`);
+  database.init(bot.channels);
+});
+
+bot.on('guildCreate', (guild) => {
+  database.addAllUsers(bot.channels);
+});
+
+bot.on('guildMemberAdd', (member) => {
+  database.addUser(member.user.id, member.user.username);
 });
 
 // Messages listener
--- database.js
+++ database.js
@@ -1,25 +1,46 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('kingdomrp.db');
  
-exports.init = () => {
-    db.run("CREATE TABLE IF NOT EXISTS user (" +
-           "id VARCHAR(45) PRIMARY KEY," +
-           "gold INTEGER NOT NULL);");
+exports.init = (channels) => {
+  db.run('CREATE TABLE IF NOT EXISTS user (' +
+         'id VARCHAR(45) PRIMARY KEY,' +
+         'username VARCHAR(45) NOT NULL,' +
+         'gold INTEGER NOT NULL);', () => {
+           this.addAllUsers(channels);
+         });
 
-    db.run("CREATE TABLE IF NOT EXISTS item (" +
-           "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-           "name VARCHAR(45) NOT NULL," +
-           "description VARCHAR(150) NOT NULL," +
-           "price INTEGER NOT NULL," +
-           "attack INTEGER NOT NULL," +
-           "defense INTEGER NOT NULL," +
-           "roll INTEGER NOT NULL," +
-           "target INTEGER NOT NULL," +
-           "from_jail BOOLEAN NOT NULL);");
+  db.run('CREATE TABLE IF NOT EXISTS item (' +
+         'id INTEGER PRIMARY KEY AUTOINCREMENT,' +
+         'name VARCHAR(45) NOT NULL,' +
+         'description VARCHAR(150) NOT NULL,' +
+         'emoji VARCHAR(45) NOT NULL,' +
+         'price INTEGER NOT NULL,' +
+         'attack INTEGER NOT NULL,' +
+         'defense INTEGER NOT NULL,' +
+         'roll INTEGER NOT NULL,' +
+         'target INTEGER NOT NULL,' +
+         'from_jail BOOLEAN NOT NULL);');
 
-    db.run("CREATE TABLE IF NOT EXISTS inventory (" +
-           "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-           "user_id VARCHAR(45) REFERENCES user(id), " +
-           "item_id INTEGER REFERENCES item(id));");
-    db.close();
+  db.run('CREATE TABLE IF NOT EXISTS inventory (' +
+         'id INTEGER PRIMARY KEY AUTOINCREMENT,' +
+         'user_id VARCHAR(45) REFERENCES user(id), ' +
+         'item_id INTEGER REFERENCES item(id));');
+};
+
+exports.addUser = (id, username) => {
+  db.run('INSERT INTO user (' +
+         'id, username, gold) VALUES (' +
+         '?, ?, 0);', id, username, () => {
+          /* error handling */
+        });
+};
+
+exports.addAllUsers = (channels) => {
+  channels.map((channel) => {
+    channel.members.map((member) => {
+      if (!member.user.bot) {
+        this.addUser(member.user.id, member.user.username);
+      }
+    });
+  })
 };
