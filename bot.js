c3303f29dd418d798c3711607ecee64ca6676f91
William Cote <william.cote@bell.net>
2017-04-28 23:05:00 -0400

    clean up for PR

--- bot.js
+++ bot.js
@@ -15,27 +15,11 @@ bot.on('ready', () => {
 
 // Messages listener
 bot.on('message', msg => {
-  if (msg.author.bot) return;
-  if (!msg.content.startsWith(config.trigger)) return;
-
-  let command = msg.content.split(" ")[0];
-  command = command.slice(config.trigger.length);
 
   // Help command
-  if (command === 'help') {
+  if (msg.content.startsWith(`${config.trigger}help`)) {
     console.log(`[${timestamp()}] ${msg.author.username} used ${config.trigger}help command.`);
     msg.reply('Here are my commands.');
-  }
-
-  // Store command
-  if (command === 'store') {
-      console.log(`[${timestamp()}] ${msg.author.username} used ${config.trigger}store command.`);
-      msg.reply(`Here\'s what you can buy:\n
-                ${emoji.get('dagger')} Donne +1 chance de tuer (2 000 ${emoji.get('moneybag')})\n
-                ${emoji.get('shield')} Donne +1 chance de survivre (2 000 ${emoji.get('moneybag')})\n
-                ${emoji.get('bow_and_arrow')} Permet de tuer à partir de la prison (3 000 ${emoji.get('moneybag')})\n
-                ${emoji.get('bomb')} Permet de tuer deux personne avec un roll (5 000 ${emoji.get('moneybag')})\n
-                ${emoji.get('four_leaf_clover')} Permet de roll (20 000 ${emoji.get('moneybag')})\n`);
   }
 });
 
